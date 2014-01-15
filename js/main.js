bcdda382289084e06311c13eb8c598ccbeb22011
maxim1994 <nok6233@gmail.com>
2014-01-15 23:40:07 +0200

    edit5

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ $(document).ready( function(){
     //carusel
     $('#bxslider').bxSlider();
     //end
+    //hover
+    /*$("#hov").hover(function(){
+        $(this).css({'cursor': 'pointer'});
+        $("#hov-1").css({'color': '#0099cc'});
+    },function(){
+        return -1;
+    });*/
+    //end hover
 });//end ready
